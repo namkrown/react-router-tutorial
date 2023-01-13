2cea8e4121d2ed0c914c9915e0617cedc2c9ecb6
Nathan Workman <nathanjonworkman@gmail.com>
2023-01-12 19:13:29 -0600

    Use  in the sidebar

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,11 @@
-import { Form, Link, Outlet, redirect, useLoaderData } from "react-router-dom";
+import {
+  Form,
+  Link,
+  NavLink,
+  Outlet,
+  redirect,
+  useLoaderData,
+} from "react-router-dom";
 import { createContact, getContacts } from "../contacts";
 
 export async function action() {
@@ -39,7 +46,12 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive ? "active" : isPending ? "pending" : ""
+                    }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -48,7 +60,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}{" "}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
